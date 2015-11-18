1fc4737d3bbc0858cf7973697bf8286375a7acbb
KHShadowrunner <khutley@gmail.com>
2015-11-18 11:59:06 -0500

    Update FFRKViewDatabase.cs
    Reverting previous changes - button does actually do something.

--- client/UI/DatabaseUI/FFRKViewDatabase.cs
+++ client/UI/DatabaseUI/FFRKViewDatabase.cs
@@ -112,13 +112,6 @@ namespace FFRKInspector.UI.DatabaseUI
             {
                 panel.Visible = (panel == mSelectedPanel);
             }
-            if (comboBox1.SelectedIndex == 1) {
-                buttonCommit.Visible = true;
-            }
-            else
-            {
-                buttonCommit.Visible = false;
-            }
         }
 
         public DatabaseModeEnum DatabaseMode
