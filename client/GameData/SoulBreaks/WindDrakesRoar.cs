d7486e8c562b2b09b9ee0f7605841c59d57099e9
Chris Moore <moorecp@gmail.com>
2016-04-20 21:21:14 -0400

    Fixed a typo in Wind Drake's Roar.

--- client/GameData/SoulBreaks/WindDrakesRoar.cs
+++ client/GameData/SoulBreaks/WindDrakesRoar.cs
@@ -12,7 +12,7 @@ namespace FFRKInspector.GameData.SoulBreaks
         public override SchemaConstants.BuddyID BuddyId { get { return SchemaConstants.BuddyID.KRILE; } }
         public override SchemaConstants.ElementID Element { get { return SchemaConstants.ElementID.Nothing; } }
         public override SchemaConstants.Formulas Formula { get { return SchemaConstants.Formulas.Magical; } }
-        public override double Multiplier { get { return 7,6; } }
+        public override double Multiplier { get { return 7.6; } }
         public override string Name { get { return "Wind Drake's Roar"; } }
     }
 }
