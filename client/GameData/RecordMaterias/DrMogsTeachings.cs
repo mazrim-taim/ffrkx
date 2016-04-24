ea6c5a09f4af7198362439c4e06f995268a08720
Chris Moore <moorecp@gmail.com>
2016-04-24 17:39:08 -0400

    Added some RMs, SBs, and Abilities that we care about with the optimizer.

---
+++ client/GameData/RecordMaterias/DrMogsTeachings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFRKInspector.GameData.RecordMaterias
+{
+    class DrMogsTeachings : RecordMateria
+    {
+        public override uint RecordMateriaId { get { return 111000022; } }
+        public override string Name { get { return "Dr. Mog's Teachings"; } }
+    }
+}
