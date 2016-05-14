4a9f6a77f5b256236b7c6d362dcd7b70ccc2876e
Chris Moore <moorecp@gmail.com>
2016-05-14 09:19:44 -0400

    Added Waterga Strike and Tornado Strike.

---
+++ client/GameData/Abilities/WatergaStrike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFRKInspector.GameData.Abilities
+{
+    class WatergaStrike : FFRKInspector.GameData.Ability
+    {
+        public override uint AbilityId { get { return 30141053; } }
+        public override SchemaConstants.AbilityCategory Category { get { return SchemaConstants.AbilityCategory.Spellblade; } }
+        public override SchemaConstants.ElementID Element { get { return SchemaConstants.ElementID.Water; } }
+        public override SchemaConstants.Formulas Formula { get { return SchemaConstants.Formulas.Physical; } }
+        public override double Multiplier { get { return 2.1; } }
+        public override string Name { get { return "Waterga Strike"; } }
+        public override int Rarity { get { return 4; } }
+    }
+}
